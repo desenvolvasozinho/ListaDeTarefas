4f8c59f9a9e9fa56d3b2ff43f4a05573f1f2fdd3
jonathansight <jonathanwilliam.ws@gmail.com>
2023-09-13 14:19:13 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const TarefaApp());
+}
+
+class TarefaApp extends StatelessWidget {
+  const TarefaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TelaTarefa(),
+    );
+  }
+}
+
+class TelaTarefa extends StatefulWidget {
+  const TelaTarefa({super.key});
+
+  @override
+  State<TelaTarefa> createState() => _TelaTarefaState();
+}
+
+class _TelaTarefaState extends State<TelaTarefa> {
+  final TextEditingController _tarefaController = TextEditingController();
+  List<String> _tarefas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarTarefas();
+  }
+
+  void _carregarTarefas() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _tarefas = prefs.getStringList('tarefas') ?? [];
+    });
+  }
+
+  void _salvarTarefas() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('tarefas', _tarefas);
+  }
+
+  void _adicionarTarefa() {
+    setState(() {
+      final novaTarefa = _tarefaController.text;
+      if (novaTarefa.isNotEmpty) {
+        _tarefas.add(novaTarefa);
+        _tarefaController.clear();
+        _salvarTarefas();
+      }
+    });
+  }
+
+  void _removerTarefa(int index) {
+    setState(() {
+      _tarefas.removeAt(index);
+      _salvarTarefas();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Tarefas'),
+      ),
+      body: ListView.builder(
+        itemCount: _tarefas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_tarefas[index]),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () => _removerTarefa(index),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Adicionar Tarefa'),
+                content: TextField(
+                  controller: _tarefaController,
+                  decoration: const InputDecoration(labelText: 'Tarefa'),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Cancelar'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Adicionar'),
+                    onPressed: () {
+                      _adicionarTarefa();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
