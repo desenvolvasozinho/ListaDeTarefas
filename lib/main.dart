237f6da9bb4e607c2fea0d00397031678ea692cb
WilliamJonathan <144833602+desenvolvasozinho@users.noreply.github.com>
2023-10-08 14:22:30 -0300

    editar

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   runApp(const TarefaApp());
 }
-//teste
 
 class TarefaApp extends StatelessWidget {
   const TarefaApp({super.key});
